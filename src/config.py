f707ff024694872a34918cae96897254da98e2f2
xiaoye_hua <huag@kth.se>
2023-04-10 10:07:40 +0000

    add data process notebok

--- src/config.py
+++ src/config.py
@@ -1,3 +1,8 @@
+import os
 
+data_dir = 'data/'
+eval_data_dir = os.path.join(data_dir, 'eval_data')
 
-eval_data_dir = 'data/eval_data'
+raw_data_dir = os.path.join(data_dir, 'raw_data')
+raw_data_session_id_dir = os.path.join(data_dir, 'raw_data_session_id')
+task1_train_eval_dir = os.path.join(data_dir, 'task1_train_eval')
